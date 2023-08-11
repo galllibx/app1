c079b5ccb90dc296633c49252d1ecc44c81776ef
galllibx <pswxwq@gmail.com>
2023-08-11 15:27:41 +0700

    Create Class Component
    Create Class Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import { Header,Content,Footer } from "./func-components";
+import Calendar from "./class-components";
 
 function App() {
-  //แบบที่ 1 กำหนดฟังก์ชันทั้งหมดที่จะแสดงในแบบอาร์เรย์
-  return (
-    [<Header/>,<Content/>,<Footer/>]
-  )
-  //แบบที่ 2 กำหนดฟังก์ชันทั้งหมดที่จะแสเงลงใน Parent เดียวกัน
-  /*
   return (
     <>
       <Header/>
+      <p><center><Calendar/></center></p>
       <Content/>
       <Footer/>
     </>
   )
-  */
 }
-
 export default App
