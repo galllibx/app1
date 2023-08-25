fbc3d4350bda94c1a1df64a8ef8c8b95cea78713
galllibx <pswxwq@gmail.com>
2023-08-25 15:42:04 +0700

    Add state-func.js
    Add state-func.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Calendar from "./class-components";
 import { Calculator2 } from "./calculator";
 import { EventData2 } from "./event-data";
 import RefsFunc from "./refs-func";
-import RefsArray from './refs-array'
+import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <EventData2/>
       <RefsFunc/>
       <RefsArray/>
+      <MessageBox/>
     </>
   )
 }
