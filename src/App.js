543898802dd8823c94a81ceb154bac5094901f4a
galllibx <pswxwq@gmail.com>
2023-09-08 14:37:28 +0700

    create context-header
    create context-header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React from 'react'
-import Banner from './banner'
-import { Header,Content,Footer } from "./func-components";
-import Calendar from "./class-components";
-import { Calculator2 } from "./calculator";
-import { EventData2 } from "./event-data";
-import RefsFunc from "./refs-func";
-import RefsArray from './refs-array';
-import MessageBox from './state-func';
+// import Banner from './banner'
+// import { Header,Content,Footer } from "./func-components";
+// import Calendar from "./class-components";
+// import { Calculator2 } from "./calculator";
+// import { EventData2 } from "./event-data";
+// import RefsFunc from "./refs-func";
+// import RefsArray from './refs-array';
+// import MessageBox from './state-func';
+import { userContext } from './context'
+import Header from'./context-header'
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <Header/>
+//       <p><center><Calendar/></center></p>
+//       <Content/>
+//       <Banner/>
+//       <Footer/>
+//       <Calculator2/>
+//       <EventData2/>
+//       <RefsFunc/>
+//       <RefsArray/>
+//       <MessageBox/>
+//     </>
+//   )
+// }
+// export default App()
+export default function App(){
   return (
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Banner/>
-      <Footer/>
-      <Calculator2/>
-      <EventData2/>
-      <RefsFunc/>
-      <RefsArray/>
-      <MessageBox/>
-    </>
-  )
+      <userContext.Provider value={'Tom Jerry'}>
+        <Header/>
+      </userContext.Provider>
+    )
 }
-export default App
 
 
