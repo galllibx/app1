9155275c3cc2b811e423670b960c71d8635b75ca
galllibx <pswxwq@gmail.com>
2023-09-08 15:38:07 +0700

    Create Content2-Header2
    Create Content2-Header2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import React from 'react'
 // import MessageBox from './state-func';
 import { userContext } from './context'
 import Header from'./context-header'
+import Content from './context-content'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
 // function App() {
 //   return (
@@ -28,9 +31,11 @@ import Header from'./context-header'
 // }
 // export default App()
 export default function App(){
+  let [user,setUser]=React.useState()
   return (
-      <userContext.Provider value={'Tom Jerry'}>
-        <Header/>
+      <userContext.Provider value={[user,setUser]}>
+        <Header2/>
+        <Content2/>
       </userContext.Provider>
     )
 }
